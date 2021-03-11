d5ecc6bc5b48c8f19a52d1a990891fb66f275ccd
Ondolin <developer@marvin-kaiser.eu>
2021-03-11 13:38:20 +0100

    added stage try files

---
+++ stage-try/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="container" style="width: 100vw; height: 100vh">
+  </div>
+</template>
+
+<script>
+
+import {initializeTorchly, torchly} from "torch-ly-js-api";
+import {initializeStage} from "../../dist/index";
+
+export default {
+  name: 'App',
+  mounted() {
+
+    initializeTorchly({backendUrl: "ws://localhost:5000/graphql", authID: "gm"});
+
+    initializeStage(torchly, "container");
+
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0px;
+}
+</style>
